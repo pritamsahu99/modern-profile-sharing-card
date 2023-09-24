da482f43a53092ab87e8850ed728db8e3492f680
pritamsahu99 <pritamsahuofficial@gmail.com>
2023-09-24 16:56:09 +0530

    update index.html file

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-const countNum = document.getElementById('countNum');
+// const countNum = document.getElementById('countNum');
  
-const decrement = () => {
-  let value = parseInt(countNum.innerText);
-  value--;
-  countNum.innerText = value;
-}
+// const decrement = () => {
+//   let value = parseInt(countNum.innerText);
+//   value--;
+//   countNum.innerText = value;
+// }
 
-const increment = () => {
-  let value = parseInt(countNum.innerText);
-  value++;
-  countNum.innerText = value;
-}
+// const increment = () => {
+//   let value = parseInt(countNum.innerText);
+//   value++;
+//   countNum.innerText = value;
+// }
 
 
-// For Profile Share Card
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
+// // For Profile Share Card
+// const modal = document.querySelector(".modal");
+// const overlay = document.querySelector(".overlay");
 
-// Modal open function
-const openModal = () => {
-  modal.classList.add("active");
-  overlay.classList.add("overlayactive");
-}
+// // Modal open function
+// const openModal = () => {
+//   modal.classList.add("active");
+//   overlay.classList.add("overlayactive");
+// }
 
-// Modal close function
-const closeModal = () => {
-  modal.classList.remove("active");
-  overlay.classList.remove("overlayactive");
-}
+// // Modal close function
+// const closeModal = () => {
+//   modal.classList.remove("active");
+//   overlay.classList.remove("overlayactive");
+// }
